f9235d36721883ade4efc3f01f00465651a9b77e
Anteater <zx1056@naver.com>
2017-10-20 13:11:23 +0900

    1020 partial, dynamic, async/await, 중급과정 끝.

--- C#/CSIntermediateTrack/CSIntermediateTrack/Program.cs
+++ C#/CSIntermediateTrack/CSIntermediateTrack/Program.cs
@@ -420,10 +420,116 @@ bool found = s.Found('z');
 
 ***************************************************/
 
+/***************************************************
+* 날짜 : 2017.10.20
+* 목표 : partial, dynamic, async await. 중급과정 끝.
+             ******* 코멘트 *******
+Partial Type
+파일 여러 개에 걸쳐 정의할 수 있는 타입(클래스, 구조체, 인터페이스).
+Code Generator가 만든 코드와 사용자가 만드는 코드를 분리하기 위해 만들어졌다.
+예를 들면, 윈폼에서는 Form UI 디자인과 관련된 Form1.designer.cs 파일과 사용자가 쓰는 Form1.cs 파일에
+동일한 클래스명을 두고 이를 partial로 선언한다. 그리고 나중에 컴파일러가 이를 합쳐 하나의 클래스로 만든다.
+정의방법은 간단함.
+partial class Class1
+{
+    public void Run() { }
+}
+partial class Class1
+{
+    public void Get() { }
+}
+
+Partial Method
+C# 3.0에서 추가된 기능. partial 기능을 타입이 아닌 메서드에 적용.
+! Partial Method는 반드시 private 메서드여야 하고, 리턴값이 void, 즉 없어야 한다.
+사용법은 마치 C에서 헤더 파일을 만들듯이 한 파일에서 메서드 몸체를 생략하고 선언부만 적은 다음,
+구현 파일에서 실제 메서드를 구현하는것.
+// file 1
+    ...
+    partial void DoThis();
+    ...
+// file 2
+    partial void DoThis()
+    {
+        Log(DateTime.Now);
+    }
+만약 코드 상에 메서드 구현 부분이 생략되고 선언부만 있게 된다면,
+C# 컴파일러가 메서드 전체를 생략해버린다.
+즉, 특정 메서드가 다른 파일에서 구현되었는가의 여부에 따라 메서드 전체를 생략할 수 있는 기능.
+
+Static Language vs Dynamic Language
+컴파일시 Type Checking을 진행하는 경우 Static Language.
+런타임시 Type을 판별하는 경우 Dynamic Language.
+C#은 Static Language이지만, C# 4.0에서 추가된 dynamic 키워드와,
+.NET Framework 4.0에 추가된 DLR(Dynamic Language Runtime)으로 Dynamic Language가 갖는 기능을 갖추고 있다.
+즉, 프레임워크가 DLR을 사용함으로써 다른 Dynamic Language를 함께 사용하는것이 가능해졌다.
+Ruby-Python 과 C#을 같이 사용할 수 있게 됐다는 말.
+
+dynamic 키워드
+컴파일러에게 변수의 Type을 체크하지 않도록 하고 런타임시 까지 해당 타입을 알 수 없음을 표시하는 키워드.
+내부적으로 dynamic 타입은 object 타입을 사용한다. 따라서 dynamic 변수는 중간에 다른 타입의 값을 가질 수 있다.
+물론 object와 다른 점도 있음.
+object o = 10;
+o = o + 20; // Error!
+o = (int)o + 20;
+object는 casting이 필요하지만 dynamic은 casting이 필요없다.
+dynamic d = 10;
+d = d + 20;
+이렇게 해도 된다는 말.
+코드를 작성하면서 봤듯이 dynamic은 런타임에 와서야 타입을 알 수 있기 때문에
+다이나믹 변수 하고 .을 타이핑 해도 비주얼스튜디오가 메서드들을 불러올 수 없다.
+
+익명타입과 dynamic
+dynamic은 컴파일러에게는 그냥 정적 Type중 하나로 인식된다. 따라서 다음과 같이 파라미터로써 지정할 수도 있다.
+    public void Run(dynamic o)
+    {
+        Console.WriteLine(o.Name);
+        Console.WriteLine(o.Age);
+    }
+그리고 dynamic t = new { Name = "Kim", Age = 25 };
+이렇게 변수 하나 만들어서 인자로 전달이 가능하다.
+문제는 익명타입 자체가 한번 생성 후 새로운 속성을 갖지 못하고,
+메서드 이벤트 등은 아예 가질 수 없기 때문에 이러한 멤버를 동적으로 할당해서 dynamic 타입에서 추가할 수 없다는것.
+또한 메서드 정의가 있는 클래스와 메서드를 호출한 클래스가 서로 다른 어셈블리에 놓인다면
+object does not contain a definition for Name 에러를 발생시킨다.
+dynamic 타입이 믹명 타입인데, 다른 어셈블리에서는 이 익명타입을 볼 수없기 때문.
+
+ExpandoObject
+이러한 문제점을 해결하기 위해 DLR 네임스페이스 System,Dynamic에 ExpandoObject 클래스가 있다.
+ExpandoObject 클래스는 개발자가 dynamic 타입에속성, 메서드, 이벤트를 동적으로 쉽게 할당할 수 있게 도와주는 클래스이다.
+아래 코드에 예제를 직접 작성해보자. (MyDyClass)
+ExpandoObject 클래스는 동적으로 추가되는 멤버들을 내부 해시테이블에 저장하고 있다.
+필요한 경우 이 정보를 IDictionary<String, Object> 인터페이스를 통해 쉽게 엑세스할 수 있다.
+말인즉슨 ExpandoObject 클래스 자체가 IDictionary<String, Object> 인터페이스를 구현하고 있고,
+클래스 캑체를 IDictionary<String, Object> 인터페이스로 캐스팅해서 내부 멤버 데이터에 엑세스 할 수 있다는것.
+    var dict = (IDictionary<string, object>)d;
+
+    foreach (var m in dict)
+    {
+        Console.WriteLine("{0}: {1}", m.Key, m.Value);
+    }
+
+async / await
+C# 5.0 부터 추가된 키워드로, 비동기 프로그래밍을 보다 손쉽게 지원하기 위해 추가되었다.
+async는 컴파일러에게 해당 메서드가 await을 가지고 있음을 알려주는 역할을 한다.
+async라고 표시된 메서드는 await을 1개 이상 가질 수 있는다. 하나도 없을때에도 컴파일이 가능하긴 하지만 warning이 뜸.
+await은 보통 Task/Task<T>객체, GetAwaiter()라는 메서드를 갖는 클래스와 함께 사용된다. await은 해당 클래스 객체가 완료되기를 기다리는데,
+!! UI 쓰레드가 정지되지 않고 메시지 루프를 계속 돌 수 있도록 필요한 코드를 컴파일러가 await 키워드를 만났을때 자동으로 추가한다.
+즉 마우스 클릭이라던가 키보드 입력같은 처리를 계속 할 수 있다는 것.
+await은 항상 특별한 Background Thread를 필요로 하지 않는다. (물론 대부분 Background Thread가 있음)
+즉, await은 해당 Task가 끝날 때 까지 기다렸다가 완료되면 바로 다음 실행문부터 실행을 이어가는데,
+해당 Task가 Worker Thread에서 돌 수도 있고, UI Thread에서 돌 수도 있다는 것.
+!! await은 Task가 어떤 스레드에서 돌던지 상관없이 Task 완료 후 await 이후의 실행문들을 디폴트로
+!! 원래 await을 실행하기 전의 스레드에서 실행하도록 보장한다.
+
+async, await 부분은 멀티쓰레딩 파트를 배우면 다시 제대로 보자.
+***************************************************/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Dynamic;
 using System.Threading.Tasks;
 using MyWorld;
 
@@ -523,6 +629,63 @@ namespace CSIntermediateTrack
         #endregion
     }
 
+    class MyDyClass
+    {
+        public void M1()
+        {
+            // ExpandoObject를 통해 dynamic 타입 생성.
+            dynamic person = new ExpandoObject();
+
+            // 속성
+            person.Name = "Kim";
+            person.Age = 10;
+
+            // 메서드. 람다식을 통한 delegate로 지정.
+            person.Display = (Action)(() =>
+            {
+                Console.WriteLine("{0} {1}", person.Name, person.Age);
+            });
+
+            person.ChangeAge = (Action<int>)((age) =>
+            {
+                person.Age = age;
+                if (person.AgeChanged != null)
+                {
+                    person.AgeChanged(this, EventArgs.Empty);
+                }
+            });
+
+            // 이벤트 초기화
+            person.AgeChanged = null; // dynamic 이벤트는 먼저 null로 초기화 한다고 한다.
+            // 이벤트핸들러
+            person.AgeChanged += new EventHandler(OnAgeChanged);
+
+            // 타 메서드에 파라미터로 전달
+            M2(person);
+        }
+
+        private void OnAgeChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Age Changed");
+        }
+
+        // dynamic 파라미터를 전달받는 메서드
+        public void M2(dynamic d)
+        {
+            // dynamic 타입 메서드 호출
+            d.Display();
+            d.ChangeAge(20);
+            d.Display();
+            Console.WriteLine("=======================");
+            var dict = (IDictionary<string, object>)d;
+
+            foreach (var m in dict)
+            {
+                Console.WriteLine("{0}: {1}", m.Key, m.Value);
+            }
+        }
+    }
+
     public static class ExClass
     {
         public static string ToChangeCase(this String str)
@@ -588,11 +751,21 @@ namespace CSIntermediateTrack
                 Console.WriteLine(under30.Name);
             */
 
-            string s = "This is a Test";
+            /*string s = "This is a Test";
 
             string s2 = s.ToChangeCase();
 
-            bool found = s.Found('z');
+            bool found = s.Found('z');*/
+
+            /*dynamic v = 1;
+            Console.WriteLine(v.GetType());
+
+            v = "string";
+            Console.WriteLine(v.GetType());*/
+
+            MyDyClass MDC = new MyDyClass();
+
+            MDC.M1();
         }
 
         #region DELEGATE
