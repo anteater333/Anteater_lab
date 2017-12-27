23aad2e530f10b076e523ac55881cdc0a57c0145
Anteater <zx1056@naver.com>
2017-12-27 21:11:47 +0900

    Chapter 11 Proxy 패턴 (1)

---
+++ C#/CSDesignPatternTrack/CSDesignPatternTrack/Remoting/Client/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Remoting;
+
+class Client
+{
+    public static void Main()
+    {
+        RemotingConfiguration.Configure("Client.exe.config", false);
+        RemotableType remoteObject = new RemotableType();
+        Console.WriteLine(remoteObject.SayHello());
+    }
+}
