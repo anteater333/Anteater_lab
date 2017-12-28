d75fbd26ad2215665343433c6f61ca61663f5039
Anteater <zx1056@naver.com>
2017-12-28 21:12:05 +0900

    Chapter 11 Proxy 패턴 (2)

---
+++ C#/CSDesignPatternTrack/CSDesignPatternTrack/Remoting/Class12/MachineService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Remoting;
+
+namespace Gumball05
+{
+    class MachineService
+    {
+        public static void Main()
+        {
+            RemotingConfiguration.Configure("MachineService.exe.config", false);
+            GumballMachineRemote gumballMachine = new GumballMachine("Seattle", 500);
+            Console.WriteLine("Location: " + gumballMachine.Location);
+            Console.WriteLine("Count: " + gumballMachine.Count);
+            RemotingServices.SetObjectUriForMarshal(gumballMachine, "Seattle.rem");
+            RemotingServices.Marshal(gumballMachine);
+            Console.ReadLine();
+        }
+    }
+}
