92cb614886fb6367a1a8474d735d9a5950b35f5b
Anteater <zx1056@naver.com>
2018-03-27 21:51:10 +0900

    GUI 디자인

---
+++ Anteater_Projects/TODOReader/TODOReader/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TODOReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.settingButton = new System.Windows.Forms.Button();
+            this.todoTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // settingButton
+            // 
+            this.settingButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.settingButton.FlatAppearance.BorderSize = 0;
+            this.settingButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingButton.Image = global::TODOReader.Properties.Resources.gear_4_16x16;
+            this.settingButton.Location = new System.Drawing.Point(408, 280);
+            this.settingButton.Name = "settingButton";
+            this.settingButton.Size = new System.Drawing.Size(26, 24);
+            this.settingButton.TabIndex = 2;
+            this.settingButton.UseVisualStyleBackColor = true;
+            // 
+            // todoTextbox
+            // 
+            this.todoTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.todoTextbox.BackColor = System.Drawing.SystemColors.Window;
+            this.todoTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.todoTextbox.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.todoTextbox.Location = new System.Drawing.Point(16, 72);
+            this.todoTextbox.Multiline = true;
+            this.todoTextbox.Name = "todoTextbox";
+            this.todoTextbox.ReadOnly = true;
+            this.todoTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.todoTextbox.Size = new System.Drawing.Size(416, 208);
+            this.todoTextbox.TabIndex = 3;
+            this.todoTextbox.Text = "임시텍스트PlaceHolder";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(450, 310);
+            this.Controls.Add(this.todoTextbox);
+            this.Controls.Add(this.settingButton);
+            this.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Bold);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Movable = false;
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(18, 68, 18, 26);
+            this.Resizable = false;
+            this.ShowIcon = false;
+            this.Text = "오늘 할 일";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button settingButton;
+        private System.Windows.Forms.TextBox todoTextbox;
+    }
+}
+
--- Anteater_Projects/TODOReader/TODOReader/MainForm.cs
+++ Anteater_Projects/TODOReader/TODOReader/MainForm.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
 
 namespace TODOReader
 {
-    public partial class Form1 : Form
+    public partial class MainForm : MetroForm
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
