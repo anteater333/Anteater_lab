e17c947e748166119c49dbf26903c90bac9d827f
Anteater on duty <zx1056@naver.com>
2018-04-18 14:03:31 +0900

    디렉토리 정리

---
+++ Node/NodeBeginnerTrack/NodeExample1/calc3.js
@@ -0,0 +1,39 @@
+const util = require('util');
+const EventEmitter = require('events').EventEmitter;
+
+/*
+const Calc = function() { // 람다식 사용시 에러 발생. 원인은 모르겠음.
+    const self = this;
+
+    this.on('stop', () => {
+        console.log('Calc에 stop event 전달됨.');
+    });
+};
+
+util.inherits(Calc, EventEmitter);
+
+Calc.prototype.add = (a, b) => {
+    return a + b;
+};
+*/
+
+// 0418 Class로 변형 시도
+
+class Calc extends EventEmitter {
+
+    constructor() {
+        super();
+        // const self = this;
+
+        this.on('stop', () => {
+            console.log('Calc에 stop event 전달됨.');
+        });
+    }
+
+    add(a, b) {
+        return a + b;
+    }
+}
+
+module.exports = Calc;
+module.exports.title = 'calculator';
