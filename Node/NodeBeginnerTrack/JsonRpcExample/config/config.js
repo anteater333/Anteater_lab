a2448c136d4c3eab7c69af4e96a31dbaf7cd2386
Anteater on duty <zx1056@naver.com>
2018-07-16 10:58:20 +0900

    Chapter 11 JSON-RPC 서버 만들기 (1)

---
+++ Node/NodeBeginnerTrack/JsonRpcExample/config/config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+    server_port : 3000
+
+    // database
+    , db_url : 'mongodb://localhost:27017/local'
+    , db_schemas: [ // Array
+        {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
+    ]
+    
+    // route
+    , route_info : [
+    ]
+
+    // facebook API
+    , facebook : {
+        clientID : '663082074030370'
+        , clientSecret : 'a4b44943fd47328c8213e8c0b1a31a09'
+        , callbackURL : '/auth/facebook/callback'
+    }
+
+    // JSON-RPC api path
+    , jsonrpc_api_path : '/api' // http://localhost:3000/api
+};
