eb3fb4b0bb316b05f33911e98f2b5181f0f499fe
Ji-hoon Lee <anteater1056@gmail.com>
2022-01-07 18:33:06 +0900

    11720 숫자의 합

---
+++ Algo/11720/app.js
@@ -0,0 +1,31 @@
+/* node.js */
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 2; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const numbers = input[1].split("");
+  let sum = 0;
+
+  numbers.map((n) => (sum += parseInt(n)));
+
+  console.log(sum);
+}
