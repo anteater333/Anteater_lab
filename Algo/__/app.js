b45e9467009255179672e31ba78625e920103556
Ji-hoon Lee <anteater1056@gmail.com>
2022-02-23 11:39:02 +0900

    템플릿 코드 미세 수정

--- Algo/__/app.js
+++ Algo/__/app.js
@@ -1,28 +1,24 @@
 /* node.js */
 
-const rl = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let input = []; // 입력 큐
 let inputNum = 2; // 입력 갯수
 
-rl.on('line', function(line) {
-    if (inputNum == -1) {
-        inputNum = line
-    }
-    else {
-        input.push(line);
-        if (input.length == inputNum) rl.close();
-    }
-}).on('close', function() {
-    
-    algorila();
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
 
-    process.exit();
+  process.exit();
 });
 
-function algorila() {
-
-}
+function algorila() {}
