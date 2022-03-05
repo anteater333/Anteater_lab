467f333c3f281620e43e8acdf8a23bbef8aa8330
Ji-hoon Lee <anteater1056@gmail.com>
2022-03-05 14:23:15 +0900

    2475 검증수 등산다녀와서 방전

---
+++ Algo/2475/app.js
@@ -0,0 +1,28 @@
+/* node.js */
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 1; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const nums = input[0].split(" ").map((a) => +a);
+
+  console.log(nums.reduce((p, c) => (p += c ** 2), 0) % 10);
+}
