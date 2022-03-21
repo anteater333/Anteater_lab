2b4b264283e2834e4c9f703b9cd2273e1cff4ad4
Ji-hoon Lee <anteater1056@gmail.com>
2022-03-21 18:05:54 +0900

    1550 16진수 우분투에게 당한 상처를 회복중

---
+++ Algo/1550/app.js
@@ -0,0 +1,26 @@
+/* node.js */
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 1; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  console.log(parseInt("0x" + input[0]));
+}
