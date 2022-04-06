114a44f9bb55f1bcfe7061f8614986b549212571
Ji-hoon Lee <anteater1056@gmail.com>
2022-04-06 10:37:21 +0900

    2338 긴자리 계산

---
+++ Algo/2338/app.js
@@ -0,0 +1,30 @@
+/* node.js */
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 2; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const [A, B] = input.map((a) => BigInt(a));
+
+  console.log((A + B).toString());
+  console.log((A - B).toString());
+  console.log((A * B).toString());
+}
