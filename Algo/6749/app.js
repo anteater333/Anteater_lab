0536cb48dc8babb826538996c16fb9937908f7b8
Ji-hoon Lee <anteater1056@gmail.com>
2022-06-18 21:58:04 +0900

    6749 Next in line

---
+++ Algo/6749/app.js
@@ -0,0 +1,33 @@
+/* node.js */
+
+// 6749 Next in line
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 2; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const theYoungest = +input[0];
+  const theMiddle = +input[1];
+
+  const theOldest = 2 * theMiddle - theYoungest;
+
+  console.log(theOldest);
+}
