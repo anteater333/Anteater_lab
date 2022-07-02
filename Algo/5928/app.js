ff15555768258b5cc4cad1db11bc12ee5c2d3136
Ji-hoon Lee <anteater1056@gmail.com>
2022-07-02 23:01:03 +0900

    5928 Contest Timing

---
+++ Algo/5928/app.js
@@ -0,0 +1,39 @@
+/* node.js */
+
+// 5928 Contest Timing
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 1; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const [
+    /** The Date */
+    D,
+    /** The Hour */
+    H,
+    /** The Minute */
+    M,
+  ] = input[0].split(" ").map((a) => +a);
+
+  const Ds = (Hs = Ms = 11);
+
+  console.log(Math.max((D - Ds) * 24 * 60 + (H - Hs) * 60 + (M - Ms), -1));
+}
