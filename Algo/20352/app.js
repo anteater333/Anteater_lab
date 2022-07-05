cb3fe3f25ec5d38651cd03a693322d19931c31ca
Ji-hoon Lee <anteater1056@gmail.com>
2022-07-05 18:22:56 +0900

    20352 Circus

---
+++ Algo/20352/app.js
@@ -0,0 +1,32 @@
+/* node.js */
+
+// 20352 Circus
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 1; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const S = +input[0];
+  const R = Math.sqrt(S / Math.PI);
+  const L = 2 * R * Math.PI;
+
+  console.log(L);
+}
