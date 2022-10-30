1da971de31964a692dc7978d12e007f3b3133ec5
Ji-hoon Lee <anteater1056@gmail.com>
2022-10-30 17:41:24 +0900

    3507 Automated Telephone Exchange

---
+++ Algo/3507/app.js
@@ -0,0 +1,30 @@
+/* node.js */
+
+// 3507 Automated Telephone Exchange
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 1; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const ate = +input[0];
+
+  console.log(Math.max(199 - ate, 0));
+}
