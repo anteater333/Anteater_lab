f7cd38fc9a625d3ac84677d3b3fdac56b4ad2571
Ji-hoon Lee <anteater1056@gmail.com>
2022-11-26 21:37:21 +0900

    4999 아!

---
+++ Algo/4999/app.js
@@ -0,0 +1,31 @@
+/* node.js */
+
+// 4999 아!
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 2; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const JH = input[0].length;
+  const Doc = input[1].length;
+
+  console.log(JH >= Doc ? "go" : "no");
+}
