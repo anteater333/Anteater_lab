61b8f05ce6f9436e692f1f8acbc579032abd26b2
Ji-hoon Lee <anteater1056@gmail.com>
2022-12-28 18:34:03 +0900

    3046 R2

---
+++ Algo/3046/app.js
@@ -0,0 +1,32 @@
+/* node.js */
+
+// 3046 R2
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // 입력 큐
+let inputNum = 1; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const [R1, S] = input[0].split(" ").map((a) => +a);
+
+  const R2 = 2 * S - R1;
+
+  console.log(R2);
+}
