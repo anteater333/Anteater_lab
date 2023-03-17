17bde758df27f4ddeb8d6246d64a4b4399353e58
Ji-hoon Lee <anteater1056@gmail.com>
2023-03-17 10:31:44 +0900

    7891 Can you add this?

---
+++ Algo/7891/app.js
@@ -0,0 +1,41 @@
+/* node.js */
+
+// 7891 Can you add this?
+
+const { createReadStream } = require("fs");
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  // input: createReadStream("./tc.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+/** @type {Array<string>} */
+let input = []; // 입력 큐
+/** @type {number} */
+let inputNum = -1; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  input.forEach((tc) => {
+    console.log(
+      tc
+        .split(" ")
+        .map((a) => +a)
+        .reduce((pV, cV) => (pV += cV))
+    );
+  });
+}
