48e5dbd7be5a81b5868b5db5625894a1df5c368e
Ji-hoon Lee <anteater1056@gmail.com>
2023-03-26 19:39:41 +0900

    1547 공

---
+++ Algo/1547/app.js
@@ -0,0 +1,42 @@
+/* node.js */
+
+// 1547 공
+
+const { createReadStream } = require("fs");
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  // input: createReadStream("./tc.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+/** @type {Array<string>} */
+let input = []; // 입력 큐
+/** @type {number} */
+let inputNum = -1; // 입력 갯수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  let ball = "1";
+
+  input.forEach((line) => {
+    const [X, Y] = line.split(" ");
+
+    ball = X === ball ? Y : Y === ball ? X : ball;
+  });
+
+  console.log(ball);
+}
