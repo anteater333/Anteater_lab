a6ec6634b28d26e9721fae29d7c9f08617e8c86a
Ji-hoon Lee <anteater1056@gmail.com>
2023-05-21 22:22:23 +0900

    2393 Rook
    스파6 하느라 일요일 날먹. 즐거웠으면 됐다...

---
+++ Algo/2393/app.js
@@ -0,0 +1,15 @@
+// 2393 Rook
+
+console.log(`  ___  ___  ___`);
+console.log(`  | |__| |__| |`);
+console.log(`  |           |`);
+console.log(`   \\_________/`);
+console.log(`    \\_______/`);
+console.log(`     |     |`);
+console.log(`     |     |`);
+console.log(`     |     |`);
+console.log(`     |     |`);
+console.log(`     |_____|`);
+console.log(`  __/       \\__`);
+console.log(` /             \\`);
+console.log(`/_______________\\`);
