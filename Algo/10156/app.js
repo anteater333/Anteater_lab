281074cbb0dd4e6014b251f327bcfacf2c6fa5f6
Ji-hoon Lee <anteater1056@gmail.com>
2023-05-28 17:53:42 +0900

    10156 과자

---
+++ Algo/10156/app.js
@@ -0,0 +1,32 @@
+/* node.js */
+
+// 10156 과자
+
+const { createReadStream } = require("fs");
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  // input: createReadStream("./tc.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+/** @type {Array<string>} */
+let input = []; // 입력 큐
+/** @type {number} */
+let inputNum = 1; // 입력 개수
+
+rl.on("line", function (line) {
+  input.push(line);
+  if (input.length == inputNum) rl.close();
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const [K, N, M] = input[0].split(" ").map((a) => +a);
+
+  console.log(Math.max(0, K * N - M));
+}
