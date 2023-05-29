dd88d145cc3e23ae6b8b3a22605c448fc565032f
Ji-hoon Lee <anteater1056@gmail.com>
2023-05-29 16:27:38 +0900

    5543 상근날드

---
+++ Algo/5543/app.js
@@ -0,0 +1,36 @@
+/* node.js */
+
+// 5543 상근날드
+
+const { createReadStream } = require("fs");
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  // input: createReadStream("./tc.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+/** @type {Array<string>} */
+let input = []; // 입력 큐
+/** @type {number} */
+let inputNum = 5; // 입력 개수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const [sang, jung, ha, coke, soda] = input.map((a) => +a);
+
+  console.log(Math.min(sang, jung, ha) + Math.min(coke, soda) - 50);
+}
