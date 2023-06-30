c5cc5f744aa3f88295c6387ec27f2f6c5966e6f3
Ji-hoon Lee <anteater1056@gmail.com>
2023-06-30 19:07:10 +0900

    6840 Who is in the middle?

---
+++ Algo/6840/app.js
@@ -0,0 +1,36 @@
+/* node.js */
+
+// 6840 Who is in the middle?
+
+const { createReadStream } = require("fs");
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  // input: createReadStream("./tc.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+/** @type {Array<string>} */
+let input = []; // 입력 큐
+/** @type {number} */
+let inputNum = 3; // 입력 개수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  const [a, b, c] = input.map((a) => +a).sort((a, b) => a - b);
+
+  console.log(b);
+}
