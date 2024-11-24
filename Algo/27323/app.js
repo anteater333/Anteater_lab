9b40732dff02c8e901aaf37497e75b5ab7d01eaf
Jihoon Lee <anteater1056@gmail.com>
2024-11-25 08:27:08 +0900

    27323 직사각형

---
+++ Algo/27323/app.js
@@ -0,0 +1,34 @@
+/* node.js */
+
+// 27323 직사각형
+
+const { createReadStream } = require("fs");
+
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  // input: createReadStream("./tc.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+/** @type {Array<string>} */
+let input = []; // 입력 큐
+/** @type {number} */
+let inputNum = 2; // 입력 개수
+
+rl.on("line", function (line) {
+  if (inputNum == -1) {
+    inputNum = line;
+  } else {
+    input.push(line);
+    if (input.length == inputNum) rl.close();
+  }
+}).on("close", function () {
+  algorila();
+
+  process.exit();
+});
+
+function algorila() {
+  console.log(+input[0] * +input[1]);
+}
